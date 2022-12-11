075cfaa2cb932fe4acb0ac789358ebd2354d21ca
claudia.sollinger@gmail.com <claudia.sollinger@gmail.com>
2022-12-11 14:21:00 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Advice Generator</h1>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@font-face {
+  font-family: "Manrope";
+  font-style: normal;
+  font-weight: 800;
+  font-display: swap;
+  src: url(./assets/xn7_YHE41ni1AdIRqAuZuw1Bx9mbZk59E9_C-bnTfc7AGrY.woff2)
+    format("woff2");
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+    U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215,
+    U+FEFF, U+FFFD;
+}
+
+:root {
+  font-family: Manrope, sans-serif;
+  font-size: 28px;
+  --color-primary-1: hsl(193, 38%, 86%);
+  --color-primary-2: hsl(150, 100%, 66%);
+  --color-neutral-ligth: hsl(217, 19%, 38%);
+  --color-neutral-dark: hsl(217, 19%, 24%);
+  --color-neutral-darkblue: hsl(218, 23%, 16%);
+}
+
+body {
+  width: 375px;
+  margin-top: 0px;
+  margin: auto;
 }
 </style>
